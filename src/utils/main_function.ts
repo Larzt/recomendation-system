fcfd465960107f8607ccd09db6715a6893cbec6f
Mario Guerra Pérez <alu0101395036@ull.edu.es>
2025-10-19 16:38:29 +0100

    main function with neighbor selection

--- src/utils/main_function.ts
+++ src/utils/main_function.ts
@@ -12,14 +12,26 @@ interface Props {
 }
 
 /// TODO: no se si poner esta funcion aqui o en otro archivo 
-export function switchAlgorithm(algorithm: AlgorithmType, Row1: number, Row2: number, Item: boolean = false): number | undefined  { 
+export function switchAlgorithm(algorithm: TAlgorithm, Row1: number, Row2: number, Item: boolean = false): number | undefined  { 
   switch (algorithm) {
-    case 'euclidea':
+    case 'euclidean':
       return euclideanDistance(Row1, Row2, Item);
     // case 'pearson':
     //   return pearsonCorrelation(Row1, Row2, Item);
     // case 'coseno':
     //   return cosineSimilarity(Row1, Row2, Item);
+    default:
+      return undefined;
+  }
+}
+export function switchPrediction(prediction: TPrediction, ) {
+  switch (prediction) {
+    case 'meanDifference':
+      // return meanDifferencePrediction();
+      break;
+    case 'simple':
+      // return simplePrediction();
+      break;
     default:
       return undefined;
   }
@@ -50,6 +62,27 @@ export function mainFunction(props: Props) {
         console.log(`Distancia entre columna ${RoworCol1} y columna ${RoworCol2}: ${distance}`);
       }
     });
+    // ordenar las distancias de mayor a menor
+    distances.sort((a, b) => b.distance - a.distance);
+    console.log('Distancias ordenadas:', distances);
+    // coger los N vecinos más cercanos
+    const neighbors = distances.slice(0, props.Neighbors); // slice para coger los N primeros elementos empezando desde el índice 0
+    // neighbors es un array de objetos con el índice y la distancia
+    console.log('Vecinos más cercanos:', neighbors);
+    // aplicar la formula de prediccion
+    // const meanCol_taget = matrixInfo.getColMean(RoworCol1 as number);
+    // let numerator = 0;
+    // let denominator = 0;
+    // neighbors.forEach((neighbor) => {
+    //   const meanCol_neighbor = matrixInfo.getColMean(neighbor.index);
+      
+    /// TODO: completar la prediccion segun el tipo (hacer llamada al switchPrediction)
+
+
+
+
+
+
 
   } else { // en caso de recorrido en filas
     // recorrer las filas
@@ -71,7 +104,17 @@ export function mainFunction(props: Props) {
         distances.push({ index: RoworCol2, distance });
         console.log(`Distancia entre fila ${RoworCol1} y fila ${RoworCol2}: ${distance}`);
       }
-    })
+    });
+    // ordenar las distancias de mayor a menor
+    distances.sort((a, b) => b.distance - a.distance);
+    console.log('Distancias ordenadas:', distances);
+    // coger los N vecinos más cercanos
+    const neighbors = distances.slice(0, props.Neighbors); // slice para coger los N primeros elementos empezando desde el índice 0
+    console.log('Vecinos más cercanos:', neighbors);
+    // aplicar la formula de prediccion
+
+    /// TODO: completar la prediccion segun el tipo (hacer llamada al switchPrediction)
   }
+  // coger los N vecinos más cercanos
   
 }
